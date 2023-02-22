ab8690f52ec50ad419132e347c48319896a7bca5
msong <minseoksong8@gmail.com>
2023-02-21 17:37:24 -0800

    Day 1. checkpoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
+import videoData from '../data/exampleVideoData.js';
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+
+//import defaultExport from _____
+console.log(videoData);
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
+        <VideoPlayer />
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+        <VideoList />
       </div>
     </div>
   </div>
+
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,23 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = () => {
+// title
+// sub-title
+// img
+// iterate over a video data array
+// while rendering with listEntry
+
+  return (
+    <div className="video-list">
+      {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
+      <VideoListEntry />
+    </div>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
